03909db5b5d6119be98924adf779c0737305b837
namoshri <rasal1710@gmail.com>
2018-04-16 07:32:09 +0530

    CarNd: Term2: P4: PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,30 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double p, double i, double d) {
+	Kp = p;
+	Ki = i;
+	Kd = d;
+
+	int_cte = 0;
+	prev_cte = 0;
+	p_error = i_error = d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+	double diff_cte = cte - prev_cte;
+	int_cte += cte;
+	prev_cte = cte;
+	p_error = -Kp * cte;
+	i_error = -Ki * int_cte;
+	d_error = -Kd * diff_cte;
 }
 
 double PID::TotalError() {
+	double total = p_error + i_error + d_error;
+	if (total > 1.0)
+		return 1.0;
+	if (total < -1.0)
+		return -1.0;
+	return total;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,12 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  // Initialize the pid variable.
+  // From lesson: 0.2, 3.0, 0.004
+  double Kp = 0.2;
+  double Ki = 0.004;
+  double Kd = 3.0;
+  pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -52,13 +57,16 @@ int main()
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
           /*
-          * TODO: Calcuate steering value here, remember the steering value is
+          * Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
-          // DEBUG
+
+	  pid.UpdateError(cte);
+	  steer_value = pid.TotalError();
+
+	  // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
